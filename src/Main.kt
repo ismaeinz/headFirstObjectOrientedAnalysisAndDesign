b5da217068bcd016d3168b8fcfacc19d587a209d
ismaeinz <hire_me_ismaein@hotmail.com>
2025-05-30 21:02:02 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//الكتاب مش كتاب تتعلم منه اوبجكت اوريتد ده كتاب تتعلم منه الديزاين
+
+fun main() {
+    val dogDoor = DogDoor()
+    val remote = Remote(dogDoor)
+//    remote.open()
+//    remote.close()
+    remote.pressButton()
+} 
