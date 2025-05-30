1320ce263628209456500bebe0e45c1070582d06
ismaeinz <hire_me_ismaein@hotmail.com>
2025-05-30 21:13:50 +0300

    Add recognizer voice to the door and close door automatically

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,41 @@
 //الكتاب مش كتاب تتعلم منه اوبجكت اوريتد ده كتاب تتعلم منه الديزاين
+import java.util.Timer
+import java.util.TimerTask
+
+data class DogDoor(var open: Boolean = false) {
+    fun open() {
+        open = true
+        println("the door is open ! ")
+        val timer = Timer()
+        val timerTask = object : TimerTask() {
+            override fun run() {
+            close()
+            }
+        }
+        timer.schedule(timerTask, 5000)
+    }
+
+    fun close() {
+        open = false; println("the door is close ! ")
+    }
+}
+
+data class Remote(val dogDoor: DogDoor) {
+    fun pressButton() {
+        if (dogDoor.open) dogDoor.close()
+        else dogDoor.open()
+    }
+}
+//BarkRecognizer
+data class BarkRecognizer(private val door: DogDoor){
+    fun recognize(bark: String){
+        println("Found Voice $bark")
+        door.open()
+    }
+}
 
 fun main() {
     val dogDoor = DogDoor()
-    val remote = Remote(dogDoor)
-//    remote.open()
-//    remote.close()
-    remote.pressButton()
+    val recognizer = BarkRecognizer(dogDoor)
+    recognizer.recognize("هو هو ")
 } 
