b2c6eb81c78778a4bc2f4793a67ccdd7f3563b8d
ismaeinz <hire_me_ismaein@hotmail.com>
2025-05-31 20:55:14 +0300

    finish

--- src/Dog.kt
+++ src/Dog.kt
@@ -2,32 +2,26 @@
 import java.util.Timer
 import java.util.TimerTask
 
-data class DogDoor(var open: Boolean = false,
-                   private var allowedBarks: MutableList<Bark> =mutableListOf<Bark>()) {
-//
-fun addAllowedBark(newBark:Bark){
-    allowedBarks +=newBark
-}
+data class DogDoor(var open: Boolean = false, private var allowedBarks: MutableList<Bark> =mutableListOf<Bark>())
+{
+    fun addAllowedBark(newBark:Bark){ allowedBarks +=newBark }
     fun getAllowedBarks()=allowedBarks
-//
     fun open() {
         open = true
         println("the door is open ! ")
         val timer = Timer()
         val timerTask = object : TimerTask() {
             override fun run() {
-            close()
+                close()
             }
         }
         timer.schedule(timerTask, 5000)
     }
-//
     fun close() {
         open = false
         println("the door is close ! ")
     }
 }
-
 //BarkRecognizer
 data class BarkRecognizer(private val dogDoor: DogDoor){
     fun recognize(bark: Bark){
@@ -35,17 +29,13 @@ data class BarkRecognizer(private val dogDoor: DogDoor){
             if(it==bark){
                 println("Found Voice ${bark.sound}")
                 dogDoor.open()
-                return
-            }
-
-    }}
-}
+                return } }} }
 //Bark => نباح
 data class Bark(val sound: String, )
-fun main() {
+fun Do() {
     val dogDoor = DogDoor()
     val recognizer = BarkRecognizer(dogDoor)
     dogDoor.addAllowedBark(Bark("صوت نباح الكلب"))
     dogDoor.addAllowedBark(Bark("صوت نباح الكلب"))
     recognizer.recognize(Bark("صوت نباح الكلب"))
-} 
+}
